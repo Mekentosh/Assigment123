946bd6511b12f2156fb8f470462261c52eb6f348
Mekentosh <m87022928044@gmail.com>
2023-04-19 21:41:06 +0600

    123445

--- src/Main.java
+++ src/Main.java
@@ -118,3 +118,39 @@ public class myArrayList<T> implements MyList<T> {
     public int size() {
         return size;
     }
+
+    public boolean contains(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(o)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void add(T item) {
+        if (size == elements.length) {
+            Object[] newElements = new Object[elements.length * 2];
+            System.arraycopy(elements, 0, newElements, 0, elements.length);
+            elements = newElements;
+        }
+        elements[size] = item;
+        size++;
+    }
+
+    public void add(T item, int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (size == elements.length) {
+            Object[] newElements = new Object[elements.length * 2];
+            System.arraycopy(elements, 0, newElements, 0, index);
+            System.arraycopy(elements, index, newElements, index + 1, size - index);
+            elements = newElements;
+        } else {
+            System.arraycopy(elements, index, elements, index + 1, size - index);
+        }
+        elements[index] = item;
+        size++;
+    }
+
